6c5bba32552b843f892d8ccba6700670546bea11
masterUNG <phrombutr@gmail.com>
2017-01-16 11:56:28 +0700

    Test by master

--- app/src/main/java/rtc/thanyalak/jaruwan/gamebetting/MySynChronize.java
+++ app/src/main/java/rtc/thanyalak/jaruwan/gamebetting/MySynChronize.java
@@ -1,8 +1,15 @@
 package rtc.thanyalak.jaruwan.gamebetting;
 
+import android.content.Context;
+
 /**
  * Created by masterUNG on 1/16/2017 AD.
  */
 
 public class MySynChronize {
+
+    //Explicit
+    private Context context;
+    private static final String urlJSON = "http://swiftcodingthai.com/pay/get_question.php";
+
 }
